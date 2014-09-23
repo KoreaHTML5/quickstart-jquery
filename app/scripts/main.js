90b249c96c1d334d781f601685618ac83c06888d
ragingwind <ragingwind@gmail.com>
2014-09-23 21:49:40 +0900

    Add more control sample

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Update slogan text by append method
 $('#slogan').append(' with jQuery').show();
 
@@ -8,8 +10,25 @@ $('.chosen-select').chosen().change(function (q, e) {
 
 // Update slogan text by input text
 $('#typing').keypress('keyup', function(e){
-	if (e.which == 13) {
+	if (e.which === 13) {
     	$('#slogan').text(this.value);
     	this.value = '';
 	}
+});
+
+// Construct input forms by iCheck
+$('#icheck-input').iCheck({
+  	labelHover: false,
+  	cursor: true,
+  	checkboxClass: 'icheckbox_minimal',
+    radioClass: 'iradio_minimal',
+	increaseArea: '20%' // optional
+});
+
+// Handle checked event fired
+$('#icheck-input input').on('ifChecked', function(e){
+  	console.log(e);
+  	if (e.type === 'ifChecked') {
+  		$('#slogan').text($(e.target).val());		
+  	}
 });
